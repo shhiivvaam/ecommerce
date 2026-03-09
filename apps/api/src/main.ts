e80f48fc9f8dcae1dc11807cd080cf863d78c955
Shivam Kumar <life.shivam2394@gmail.com>
2026-03-10 00:23:33 +0530

    fixing of payment related req

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,7 +11,7 @@ import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exce
 import { SanitizedValidationPipe } from './common/pipes/sanitized-validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
   const logger = new Logger('Bootstrap');
 
   // 1. Security & Middleware
