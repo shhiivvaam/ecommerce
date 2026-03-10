8f8c3b2b8d8317e4f99b1c20ef0a2248cb700c1e
Shivam Kumar <life.shivam2394@gmail.com>
2026-03-10 14:39:15 +0530

    admi nrelated web changes

--- apps/web/app/admin/products/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProductForm } from "@/components/admin/ProductForm";
-
-export default function NewProductPage() {
-    return (
-        <div className="container mx-auto py-4">
-            <ProductForm />
-        </div>
-    );
-}
