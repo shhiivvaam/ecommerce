3e0cd8f8bfa8657858e5b3b19f58fa1c7a8def2f
Shivam Kumar <life.shivam2394@gmail.com>
2026-03-13 11:00:52 +0530

    making lastname optional and addinf strict types in all

--- apps/api/src/addresses/dto/create-address.dto.ts
+++ apps/api/src/addresses/dto/create-address.dto.ts
@@ -1,15 +1,31 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsString, IsBoolean, IsOptional } from 'class-validator';
+import {
+  IsString,
+  IsBoolean,
+  IsOptional,
+  MaxLength,
+  Matches,
+  IsNumberString,
+  Length,
+} from 'class-validator';
 
 export class CreateAddressDto {
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(50)
+  @Matches(/^[A-Za-z\s]*$/, {
+    message: 'firstName must contain only alphabets',
+  })
   firstName?: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(50)
+  @Matches(/^[A-Za-z\s]*$/, {
+    message: 'lastName must contain only alphabets',
+  })
   lastName?: string;
 
   @ApiPropertyOptional()
@@ -20,26 +36,37 @@ export class CreateAddressDto {
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @IsNumberString()
+  @Length(10, 10, { message: 'phone must be exactly 10 digits' })
   phone?: string;
 
   @ApiProperty()
   @IsString()
+  @MaxLength(100)
   street: string;
 
   @ApiProperty()
   @IsString()
+  @MaxLength(50)
+  @Matches(/^[A-Za-z\s]+$/, { message: 'city must contain only alphabets' })
   city: string;
 
   @ApiProperty()
   @IsString()
+  @MaxLength(50)
+  @Matches(/^[A-Za-z\s]+$/, { message: 'state must contain only alphabets' })
   state: string;
 
   @ApiProperty()
   @IsString()
+  @MaxLength(50)
+  @Matches(/^[A-Za-z\s]+$/, { message: 'country must contain only alphabets' })
   country: string;
 
   @ApiProperty()
   @IsString()
+  @IsNumberString()
+  @Length(6, 6, { message: 'zipCode must be exactly 6 digits' })
   zipCode: string;
 
   @ApiPropertyOptional()
